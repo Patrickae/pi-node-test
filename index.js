91cf96fc1f76f24218b36d245bb3040ce756493f
Andrew Patrick <andyepatrick@gmail.com>
2018-05-18 17:27:21 -0400

    update

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
 var inquirer = require('inquirer');
 var weather = require('weather-js');
-var gpio = require('rpi-gpio');
-gpio.setup(7, gpio.DIR_IN, readInput);
+var Gpio = require('onoff').Gpio;
+var button = new Gpio(17, 'in', 'both');
 
-function start() {
-	inquirer
-		.prompt([
-			{
-				message: 'Where do you want to check the weather?',
+button.watch(function (err, value) {
+  if (err) {
+    throw err;
+  }
 
-				name: 'location',
-			},
-		])
-		.then(function(answer) {
-			weather.find({ search: answer.location, degreeType: 'F' }, function(err, result) {
-				if (err) console.log(err);
-				console.log('currently it is ' + result[0].current.temperature + ' degrees');
-				console.log(
-					'tomorrow, ' +
-						result[0].forecast[1].day +
-						', has a high of ' +
-						result[0].forecast[1].high +
-						' and a low of ' +
-						result[0].forecast[1].low
-				);
-			});
-		});
-}
+  console.log(value)
+});
 
-function readInput() {
-    gpio.read(7, function(err, value) {
-        console.log('The value is ' + value);
-    });
-}
 
-var switch = 0;
-
-var piInterval = setInterval(readInput, 100)
-
+process.on('SIGINT', function () {
+  button.unexport();
+});
