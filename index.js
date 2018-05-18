277c94c4e904a541d6679dd0aa8a02ea29460f26
Andy Patrick <andyepatrick@gmail.com>
2018-05-18 18:42:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var inquirer = require('inquirer');
 var weather = require('weather-js');
-const Gpio = require('pigpio').Gpio;
+var Gpio = require('pigpio').Gpio,
+  button = new Gpio(4, {
+    mode: Gpio.INPUT,
+    pullUpDown: Gpio.PUD_DOWN,
+    edge: Gpio.EITHER_EDGE
+  })
 
-// Calling export with a pin number will export that header and return a gpio header instance
-var gpio.export(pin, {ready: fn});
-
-
-const button = new Gpio(4, {mode: Gpio.INPUT,pullUpDown: Gpio.PUD_DOWN,edge: Gpio.EITHER_EDGE});
-
-button.on('change', function (level) {
+button.on('interrupt', function (level) {
   console.log(level)
 });
