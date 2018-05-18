672bf551955bc64a1e09734efbe937e0e5b7b532
Andrew Patrick <andyepatrick@gmail.com>
2018-05-18 18:03:27 -0400

    npm update

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 var inquirer = require('inquirer');
 var weather = require('weather-js');
-var Gpio = require('onoff').Gpio;
-var button = new Gpio(17, 'in', 'both');
+var gpio = require("gpio");
 
-button.watch(function (err, value) {
-  if (err) {
-    throw err;
-  }
+// Calling export with a pin number will export that header and return a gpio header instance
+var gpio4 = gpio.export(4, {
+   // When you export a pin, the default direction is out. This allows you to set
+   // the pin value to either LOW or HIGH (3.3V) from your program.
+   direction: 'in',
 
-  console.log(value)
+   // set the time interval (ms) between each read when watching for value changes
+   // note: this is default to 100, setting value too low will cause high CPU usage
+   interval: 200,
+
+   // Due to the asynchronous nature of exporting a header, you may not be able to
+   // read or write to the header right away. Place your logic in this ready
+   // function to guarantee everything will get fired properly
+   ready: function() {
+    console.log('all good')
+   }
 });
 
-
-process.on('SIGINT', function () {
-  button.unexport();
+gpio4.on("change", function(val) {
+   // value will report either 1 or 0 (number) when the value changes
+   console.log(val)
 });
+
